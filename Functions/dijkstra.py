b916151ab9445e9821f74355e4a07d8fd45c1df7
Afmc07 <afmc07@hotmail.com>
2022-05-05 09:02:20 -0300

    Added visualization, letter label support WIP

--- Functions/dijkstra.py
+++ Functions/dijkstra.py
@@ -1,10 +1,14 @@
 import sys
 from Classes.model import model
+from .visualizer import visualizers as vs
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 def dijkstra(graph:model, sourceIdx:int):
     vertex_amount = graph.getverts()
     distances = [sys.maxsize] * vertex_amount
     distances[sourceIdx] = 0
+    road_map = {}
 
     for cout in range(vertex_amount):
         x = __minDistance(distances, graph)
@@ -13,11 +17,15 @@ def dijkstra(graph:model, sourceIdx:int):
             continue
 
         graph.setVisited(x)
+        road_map[x] = []
 
         for yIdx in range(vertex_amount):
             if int(graph.weights[x][yIdx]) > 0 and not graph.visited[yIdx] and distances[yIdx] > distances[x]+graph.weights[x][yIdx]:
                 distances[yIdx] = distances[x]+graph.weights[x][yIdx]
-    __printResult(distances, sourceIdx)            
+                mapInsert(road_map, x, yIdx)
+    alph = input("are the vertex labels letters? Y/N: ")
+    __printResult(distances, sourceIdx, alph)
+    vs.WeightedMapVisualizer(road_map, "dot", graph.weights, alph)            
 
 def __minDistance(dist:list, graph:model):
     min = sys.maxsize
@@ -29,7 +37,17 @@ def __minDistance(dist:list, graph:model):
             min_index = idx
     return min_index
 
-def __printResult(distance_list:list, startIdx:int):
+def mapInsert(road:dict, parentIdx:int, addedIdx:int):
+    keys = road.keys()
+    for key in keys:
+        if key == parentIdx:
+            continue
+        elif addedIdx in road[key]:
+            road[key].remove(addedIdx)
+            break
+    road[parentIdx].append(addedIdx)
+
+def __printResult(distance_list:list, startIdx:int, alph):
     no_road = sys.maxsize
     no_road_vec = [i for i, x in enumerate(distance_list) if x == no_road]
 
@@ -42,5 +60,8 @@ def __printResult(distance_list:list, startIdx:int):
         if index == startIdx:
             continue
         else:
-            print(f'From {startIdx+1} to {index+1}: {distance_list[index]}')
+            if alph == 'Y':
+                print(f'From {alphabet[startIdx]} to {alphabet[index]}: {distance_list[index]}')
+            else:
+                print(f'From {startIdx+1} to {index+1}: {distance_list[index]}')  
     print("\n---------------------------------\n")        
--- Functions/visualizer.py
+++ Functions/visualizer.py
@@ -5,6 +5,8 @@ from networkx.drawing.nx_pydot import graphviz_layout
 import os
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
 class GraphVisualization:
    
     def __init__(self):
@@ -15,7 +17,6 @@ class GraphVisualization:
         self.visual.append(temp)
 
     def visualize(self, type:str):
-        #G = nx.DiGraph()
         ans = input("Is Graphviz working? Y/N ")
         G = nx.Graph() if ans == "N" else nx.DiGraph()
         G.add_edges_from(self.visual)
@@ -26,13 +27,50 @@ class GraphVisualization:
             nx.draw_networkx(G)
         plt.show()
 
+    def visualizeWeighted(self, type:str, labels, alph):
+        ans = input("Is Graphviz working? Y/N ")
+        G = nx.Graph() if ans == "N" else nx.DiGraph()
+        G.add_edges_from(self.visual)
+
+        pos = nx.spring_layout(G)
+
+        if ans == 'Y':
+            nx.draw_networkx(G, graphviz_layout(G, prog=type))
+        else:
+            nx.draw_networkx(G, pos)
+            if alph == 'Y':
+                edge_labels = dict([((n1, n2), f'{labels[alphabet.index(n1)][alphabet.index(n2)]}')
+                    for n1, n2 in G.edges])
+            else:
+                edge_labels = dict([((n1, n2), f'{labels[n1][n1]}')
+                    for n1, n2 in G.edges])
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        plt.show()
+
 class visualizers:
     def MapVisualizer(connections:dict, type:str):
         visual = GraphVisualization()
         keys = connections.keys()
 
+        alph = input("are the vertex labels letters? Y/N: ")
         for key in keys:
             for item in connections[key]:
-                visual.addEdge(key+1, item+1)
+                if alph == 'Y':
+                    visual.addEdge(alphabet[key], alphabet[item])
+                else:
+                    visual.addEdge(key+1, item+1)
 
-        visual.visualize(type)
+        visual.visualize(type)  
+
+    def WeightedMapVisualizer(connections:dict, type:str, labels, alph):
+        visual = GraphVisualization()
+        keys = connections.keys()
+        
+        for key in keys:
+            for item in connections[key]:
+                if alph == 'Y':
+                    visual.addEdge(alphabet[key], alphabet[item])
+                else:
+                    visual.addEdge(key+1, item+1)
+
+        visual.visualizeWeighted(type, labels, alph)  
